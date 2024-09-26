598bc921da5010c0df8beeb4c596aee082320207
Manish <tamangmanish3456@gmail.com>
2024-09-26 20:23:18 +0545

    Fix max age comment

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,7 +51,7 @@ const setCookies = (res, accessToken, refreshToken) => {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict", // prevent CSRF attack, cross-site request forgery attack
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 15 minutes
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
 
