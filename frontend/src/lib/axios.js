f2a6f3c5c401720b077c6e86273fff65042e65bc
Manish <tamangmanish3456@gmail.com>
2024-10-07 20:22:46 +0545

    add axios instance

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL:
+    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+  withCredentials: true, // Send cookies with requests
+});
+
+export default axiosInstance;
